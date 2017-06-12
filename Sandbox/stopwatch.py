3307ef09779fae684d389bbfdff2d989ec08fcac
fanch-luck <fch.luck@qq.com>
2017-06-12 22:50:36 +0800

    change code with python3.5

--- Sandbox/stopwatch.py
+++ Sandbox/stopwatch.py
@@ -1,4 +1,4 @@
-from Tkinter import *
+from tkinter import *
 import time
 
 class StopWatch(Frame):  
--- TK Widgets/ledWidget.py
+++ TK Widgets/ledWidget.py
@@ -1,4 +1,5 @@
-from Tkinter import *
+from tkinter import *
+
 
 class LED(Frame):
     OFF_STATE = 0
@@ -19,6 +20,7 @@ class LED(Frame):
     def drawcircle(self,x,y,rad):
         color="red"
         return self.c.create_oval(x-rad,y-rad,x+rad,y+rad,width=rad/5,fill=color,outline='black')
+
     def change_color(self):
         if self.state == LED.ON_STATE:
             color="green"
@@ -27,6 +29,7 @@ class LED(Frame):
         else:
             color="red"
         self.c.itemconfig(self.led, fill=color)
+
     def toggle(self,event):
         if self.state == LED.OFF_STATE:
             self.set(LED.REQUESTED_ON)
@@ -38,35 +41,42 @@ class LED(Frame):
             self.set(LED.ON_STATE)
         else:
             self.set(LED.OFF_STATE)
-        ##self.led = self.drawcircle(self.size/2,self.size/2,self.size/2-10)
+
+        # self.led = self.drawcircle(self.size/2,self.size/2,self.size/2-10)
         self.change_color()
         self.start()
+
     def set(self,state):
         self.state = state
-        ##self.led = self.drawcircle(self.size/2,self.size/2,self.size/2-10)
+        # self.led = self.drawcircle(self.size/2,self.size/2,self.size/2-10)
+
     def get(self):
         return self.state
+
     def start(self):
         self._timer = self.after(5000, self._checkRequests)
+
     def _checkRequests(self):
-        print self.state
+        print(self.state)
         if self.state == LED.REQUESTED_ON:
             self.state = LED.ON_STATE
         elif self.state == LED.REQUESTED_OFF:
             self.state = LED.OFF_STATE
-        #self.led = self.drawcircle(self.size/2,self.size/2,self.size/2-10)
+
+        # self.led = self.drawcircle(self.size/2,self.size/2,self.size/2-10)
         self.change_color()
+
 
 def main():
     root = Tk()
     global leds
     leds = []
     size = 50
-    ##for i in range(8):
-    ##    leds.append(LED(root,size))
-    ##    #leds[i].config(command=action)
-    ##    leds[i].grid(row=0,column=i,padx=size/5,pady=size/5)
-    ##    leds[i].start()*/
+    # for i in range(8):
+    # leds.append(LED(root,size))
+    # #leds[i].config(command=action)
+    # leds[i].grid(row=0,column=i,padx=size/5,pady=size/5)
+    # leds[i].start()*/
     led = LED(root,size)
     led.grid()
     root.mainloop()
